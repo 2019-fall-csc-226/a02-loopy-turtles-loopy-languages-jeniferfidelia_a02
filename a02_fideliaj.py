fb0f64bf8908c11363e80d660221fd97964beea2
jenifidelia <fideliaj@berea.edu>
2019-09-11 15:27:40 -0400

    initial commit

---
+++ a02_fideliaj.py
@@ -0,0 +1,30 @@
+######################################################################
+# Author: Jenifer Fidelia             TODO: Change this to your name, if modifying
+# Username: Fideliaj                TODO: Change this to your username, if modifying
+#
+
+# Assignment: A02: Exploring Turtles in Python
+# Purpose: Practice using turtles and loops
+######################################################################
+# licensed under a Creative Commons
+# Attribution-Noncommercial-Share Alike 3.0 United States License.
+######################################################################
+
+import turtle
+
+#make window and add attributes
+
+#make two turtles and add attributes
+
+
+#draw your drawing (nested loops)
+
+
+
+
+
+
+
+
+
+
